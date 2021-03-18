b161cf411d93dfe07dff15a58cf10b9a9574995e
Radwan Rashed <radrash.ym@gmail.com>
2021-03-18 23:29:46 +0800

    Commit Location file

---
+++ samples/src/samples/demo/Lcation.java
@@ -0,0 +1,28 @@
+package samples.demo;
+
+public class Lcation {
+	
+	private String name;
+	private int age;
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+
+}
